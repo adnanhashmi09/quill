351a2a41bb94b2d81648ca14fbb0c7774abef98a
Adnan Hashmi <hashmi.adnan791@gmail.com>
2024-01-29 19:49:44 +0530

    got h and l key to move to next or previous line when at the end of the current line
    Signed-off-by: Adnan Hashmi <hashmi.adnan791@gmail.com>

--- main.c
+++ main.c
@@ -66,6 +66,7 @@ int main() {
     mvprintw(row - 2, 0, "row_cnt -> %d ", buffer.row_cnt);
     mvprintw(row - 3, 0, "x -> %d, y -> %d ", x, y);
     mvprintw(row - 4, 0, "row_size -> %d ", buffer.rows[buffer.row_index].size);
+    mvprintw(row - 5, 0, "row_index -> %d ", buffer.row_index);
 
     for (size_t i = 0; i < buffer.row_cnt; i++) {
       mvprintw(i, 0, buffer.rows[i].contents);
@@ -199,14 +200,14 @@ void handle_normal_mode(int ch, Buffer *buffer, int *y, int *x) {
    *  Currently I am moving to where \n character is. Editing from there will
    * overwrite newline So we need to push the last character to the end
    */
-
+  Row *curr_row;
   switch (ch) {
   case 'j':
     if (*y >= buffer->row_cnt - 1)
       break;
 
     if (*x > buffer->rows[++buffer->row_index].size - 1) {
-      Row *curr_row = &buffer->rows[buffer->row_index];
+      curr_row = &buffer->rows[buffer->row_index];
       *x = curr_row->size - 1;
       char last_char = curr_row->contents[curr_row->size - 1];
 
@@ -220,7 +221,7 @@ void handle_normal_mode(int ch, Buffer *buffer, int *y, int *x) {
     if (*y <= 0)
       break;
 
-    if (*x  + 1 > buffer->rows[--buffer->row_index].size - 1) {
+    if (*x + 1 > buffer->rows[--buffer->row_index].size - 1) {
       if (buffer->rows[buffer->row_index].size <= 1) {
         *x = 0;
       } else {
@@ -231,13 +232,34 @@ void handle_normal_mode(int ch, Buffer *buffer, int *y, int *x) {
     move(*y - 1, *x);
     break;
   case 'h':
-    if (*x <= 0)
+    if (*x <= 0 && *y <= 0)
       break;
+
+    if (*x <= 0) {
+      curr_row = &buffer->rows[--buffer->row_index];
+      *x = curr_row->size - 1;
+      *x = *x > 0 ? *x - 1 : 0;
+      buffer->curr_pos = *x;
+      move(*y - 1, *x);
+      break;
+    }
 
     move(*y, *x - 1);
     buffer->curr_pos -= 1;
     break;
   case 'l':
+    curr_row = &buffer->rows[buffer->row_index];
+    if (*x >= curr_row->size - 1 && buffer->row_index == buffer->row_cnt - 1)
+      break;
+    else if (*x+1 >= curr_row->size - 1 &&
+             buffer->row_index < buffer->row_cnt - 1) {
+      *x = 0;
+      *y += 1;
+      buffer->row_index += 1;
+      buffer->curr_pos = 0;
+      move(*y, *x);
+      break;
+    }
     move(*y, *x + 1);
     buffer->curr_pos += 1;
     break;
