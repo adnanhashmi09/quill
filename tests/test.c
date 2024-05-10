d25b252638d6fd907089f9066dfe93f13496ad39
Adnan Hashmi <hashmi.adnan791@gmail.com>
2024-05-10 23:34:26 +0530

    write more tests for gap_buffers

--- tests/test.c
+++ tests/test.c
@@ -11,5 +11,19 @@ int main() {
           &test_move_cursor_left_at_zero);
   addTest("Test insert_character() function",
           &test_insert_character_in_empty_buffer);
+  addTest("insert_character() should grow buffer when inserted beyond limit",
+          &test_buffer_should_grow_when_inserted_characters_beyond_capacity);
+  addTest("backspace() should shrink buffer when one fourth of the capacity is "
+          "being used",
+          &test_backspace_for_shrinking_buffer);
+  addTest(
+      "backspace() ran multiple times should shrink appropriately if empty "
+      "buffer is still significantly larger."
+      "being used",
+      &test_multiple_backspace_on_buffer_significantly_larger_than_required_size);
+  addTest("Test delete_character() when at the end of the buffer",
+          &test_delete_character_on_gap_end);
+  addTest("Test delete_character() on overspaced buffer",
+          &test_delete_character_on_overspaced_buffer);
   runTests();
 }
